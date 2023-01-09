75603e085728b4adb6b7bf1d11a1f19c1124e183
elina <aelin6277@gmail.com>
2023-01-09 01:00:06 +0100

    Initial Commit

---
+++ MealRoulette/MealRouletteApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealRouletteApp.swift
+//  MealRoulette
+//
+//  Created by Elin A on 2023-01-09.
+//
+
+import SwiftUI
+
+@main
+struct MealRouletteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
