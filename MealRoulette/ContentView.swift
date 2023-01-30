09024e349a3e2558c43ba49b2bf4bdadd7efabca
elina <aelin6277@gmail.com>
2023-01-30 12:10:43 +0100

    Scrollbar lista med pizzor, tryck på pizzans namn och få ino om dess innehåll längst ner på sidan. Kan klicka fram och tillbaka mellan pizzorna, kan även trycka på Refreshknappen för att få bort infpormation om den senaste pizzan so mdu tryckt på.

--- MealRoulette/ContentView.swift
+++ MealRoulette/ContentView.swift
@@ -5,32 +5,83 @@
 //  Created by Elin A on 2023-01-09.
 //
 
+
+
 import SwiftUI
 
 struct ContentView: View {
-    
-    
-    var body: some View {
+        @State var title: String = "Pizza menu"
+        @State var text: String = ""
         
-        ZStack {
-            Color(red: 251/256, green: 255/256, blue: 0/256)
-                .ignoresSafeArea() //color yellow all over the screen
-            Image("risoniPastaSallad")
-                .resizable()
-                .scaledToFill()
-                .frame(width: 300, height: 200)
-            VStack {
-                Text("Meal selection")
-                    .font(.title)
-                    .bold()
-                Spacer()
-            
-            
-                Button("Press here") {
+        var body: some View {
+            ScrollView {
+                VStack(spacing: 20) {
+                    //Text(title)
+                    Button(action: {
+                        self.title = "Tomatsås, ost"
+                    }, label: {
+                        Text("Margherita")
+                    })
+                    .accentColor(.purple)
+                    Image("Margherita")
+                    
+                    
+                    Button(action: {
+                        self.title = "Tomatsås, ost, champinjoner"
+                    }, label: {
+                        Text("Funghi")
+                        //Image("Funghi")
+                    })
+                    .accentColor(.pink)
+                    Image("Funghi")
+                    
+                    
+                    Button(action: {
+                        self.title = "Tomatsås, ost, champinjoner, skinka"
+                    }, label: {
+                        Text("Capricciosa")
+                    })
+                    Image("Capricciosa")
+                  
+                    
+                    
+                    Button(action: {
+                        self.title = "Tomatsås, mozzarella, kronärtskocka, ruccola, rödlök, avokado. Går att få vegansk."
+                    }, label: {
+                        Text("Arti")
+                    })
+                    .accentColor(.green)
+                    Image("Arti")
+                    
                     
                 }
+                
             }
+            Text(title)
+            Button(action: {
+                
+                self.title = "Välj en pizza för att se ingredienser"
+            },
+                   label: {
+                Text("Refresh".uppercased())
+                    .font(.headline)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .padding()
+                    .padding(.horizontal, 30)
+                    .background(Color.cyan)
+                    .cornerRadius(20)
+                    .shadow(radius: 20)
+                
+                
+            })
         }
     }
-}
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
 
